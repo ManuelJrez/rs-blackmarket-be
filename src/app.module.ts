2e9b4353e9cee9627cddb489a012b1f8f8cc327b
Luis Juarez <luisjuarez@Luiss-MacBook-Air.local>
2023-03-01 09:25:43 -0600

    feat: added adminjs to manage resources from a back office panel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,66 @@
+import * as AdminJSPrisma from '@adminjs/prisma';
+import { PrismaService } from './prisma/prisma.service';
+import AdminJS from 'adminjs';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+
+import { DMMFClass } from '@prisma/client/runtime';
+import { AdminModule } from '@adminjs/nestjs';
+import { PrismaClient } from '@prisma/client';
+
+AdminJS.registerAdapter({
+  Resource: AdminJSPrisma.Resource,
+  Database: AdminJSPrisma.Database,
+});
 
 @Module({
-  imports: [],
+  imports: [
+    AdminModule.createAdminAsync({
+      useFactory: () => {
+        const prisma = new PrismaClient();
+        const dmmf = (prisma as any)._baseDmmf as DMMFClass;
+        return {
+          adminJsOptions: {
+            rootPath: '/admin',
+            resources: [
+              {
+                resource: { model: dmmf.modelMap.products, client: prisma },
+                options: {
+                  navigation: {
+                    name: null,
+                    icon: null,
+                  },
+                },
+              },
+              {
+                resource: {
+                  model: dmmf.modelMap.category_products,
+                  client: prisma,
+                },
+                options: {
+                  navigation: {
+                    name: null,
+                    icon: null,
+                  },
+                },
+              },
+              {
+                resource: { model: dmmf.modelMap.categories, client: prisma },
+                options: {
+                  navigation: {
+                    name: null,
+                    icon: null,
+                  },
+                },
+              },
+            ],
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
