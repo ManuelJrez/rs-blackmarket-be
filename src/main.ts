3bd3c81b28faf9f26e689b733bc246fb4e1d7678
Luis Juarez <luisjuarez@Luiss-MacBook-Air.local>
2023-03-01 09:26:18 -0600

    fix: big int error from prisma

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// PATCH TO FIX A PROBLEM WITH PRISMA AND BIG INT
+// https://github.com/GoogleChromeLabs/jsbi/issues/30#issuecomment-1017073129
+BigInt.prototype['toJSON'] = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
